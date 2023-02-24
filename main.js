cecb6526e43fb06e0ce6346125ae0759ffa868d1
Tasfiha Saba <77076887+tasfihasaba@users.noreply.github.com>
2023-02-24 14:33:40 -0500

    Add style file

--- main.js
+++ main.js
@@ -1,65 +1,58 @@
-console.log('Your JS is linked up. Be the person you needed when you were little.')
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ];
 
-/*----- constants -----*/
-/*----- app's state (variables) -----*/
 let board;
+let turn = 'X';
+let win;
+
+const squares = Array.from(document.querySelectorAll('#board div'));
+
+document.getElementById('board').addEventListener('click', handleTurn);
+const messages = document.querySelector('h2');
+document.getElementById('reset-button').addEventListener('click', init);
+
+
+
+function getWinner() {
+    let winner = null;
+    winningCombos.forEach(function(combo, index) {
+        if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) winner = board[combo[0]];
+        });
+        return winner ? winner : board.includes('') ? null : 'T';
+};
+
+function handleTurn() {
+    let idx = squares.findIndex(function(square) {
+        return square === event.target;
+    });
+    board[idx] = turn;
+    turn = turn === 'X' ? 'O' : 'X';
+    win = getWinner();
+    render();
+};
+
 function init() {
     board = [
     '', '', '',
     '', '', '',
     '', '', ''
-    ]; 
+    ];
+    render();
 };
-    //be sure to call the init function!
-    init();
-    
-
-
-    function render(){
-        board.forEach(function(mark, index){
-            console.log(mark, index);
-            });
-    };
-
-
-
-    function init() {
-        board = [
-        '', '', '',
-        '', '', '',
-        '', '', ''
-        ];
-        // new code here
-        render();
-    };
-
-    let turn = 'X';
-/*----- cached element references -----*/
-const squares = Array.from(document.querySelectorAll('#board div'));
-const messages = document.querySelector('h2');
 
 function render() {
     board.forEach(function(mark, index) {
-    //this sets the text content of the square of the same position to the mark on the board. 
     squares[index].textContent = mark;
     });
+    messages.textContent = win === 'T' ? `That's a tie!` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
     };
-/*----- event listeners -----*/
-document.getElementById('board').addEventListener('click', handleTurn);
-/*----- functions -----*/
-function handleTurn() {
-    let idx = squares.findIndex(function(square) {
-    return square === event.target;
-    });
-    board[idx] = turn;
-    // This is tidy
-    turn = turn === 'X' ? 'O' : 'X';
-    // In an if statement it would look like: 
-    // if (turn === 'X') {
-    // turn = 'O' 
-    // } else {
-    // turn = 'X' 
-    // };
-    // writing the ternary saved you from all that. 
-    render();
-    };
+
+init();
